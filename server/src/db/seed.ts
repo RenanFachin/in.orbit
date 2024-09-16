53a75f984e6fb2d54244b4162170c23a9991ed5d
Renan <renan.fachin@hotmail.com>
2024-09-15 23:28:07 -0300

    feat: Cria seed para os objetivos concluidos

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,11 +1,12 @@
 import { client, db } from "."
 import { goalCompletions, goals } from "./schema"
+import dayjs from 'dayjs'
 
 async function seed(){
   await db.delete(goalCompletions)
   await db.delete(goals)
 
-  await db.insert(goals).values([
+  const result = await db.insert(goals).values([
     {
       title: 'Treinar',
       desiredWeeklyFrequency: 4
@@ -13,6 +14,21 @@ async function seed(){
     {
       title: 'Tomar 3L de água',
       desiredWeeklyFrequency: 7
+    }
+  ]).returning()
+  //.returning() faz com que os dados sejam retornados e nos permite armazenar em uma const
+
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalCompletions).values([
+    {
+      goalId: result[0].id,
+      createdAt: startOfWeek.toDate()
+    },
+    {
+      goalId: result[1].id,
+      createdAt: startOfWeek.add(1, 'day').toDate()
     }
   ])
 }
